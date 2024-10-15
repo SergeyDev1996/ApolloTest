ca0eb5e7dc0bfb212193006d4198caaef78d5956
Serhiy Zhyhadlo <sergeyz.dev1996@gmail.com>
2024-10-15 18:05:21 +0300

    flake8 fixes

--- loginapp/serializers.py
+++
@@ -1,3 +0,0 @@
-# serializers.py
-from rest_framework import serializers
-
--- loginapp/views.py
+++ loginapp/views.py
@@ -10,16 +10,20 @@ from rest_framework import status
 from rest_framework.views import APIView
 from seleniumwire import webdriver as wire_webdriver
 
-
-from ApolloTest.settings import APOLLO_PASSWORD, APOLLO_EMAIL, APOLLO_LOGIN_URL, APOLLO_API_REQUEST_URL, \
+from ApolloTest.settings import (
+    APOLLO_PASSWORD, APOLLO_EMAIL,
+    APOLLO_LOGIN_URL, APOLLO_API_REQUEST_URL,
     APPOLLO_API_ENDPOINT_TO_FIND
+)
 from loginapp.models import RequestData
 
 
 class LoginAndSaveCookiesView(APIView):
     chrome_options = Options()
     chrome_options.add_argument(
-        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36")
+        "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+        " AppleWebKit/537.36 (KHTML, like Gecko)"
+        " Chrome/129.0.0.0 Safari/537.36")
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
 
@@ -32,12 +36,21 @@ class LoginAndSaveCookiesView(APIView):
             time.sleep(10)  # Wait for the login page to load
             # Find and fill in the email field using XPath
             email_field = driver.find_element(By.XPATH,
-                                              '/html/body/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[2]/div/form/div[2]/div/div/input')
+                                              '/html/body/div[2]'
+                                              '/div/div[2]'
+                                              '/div/div[1]/'
+                                              'div/div[2]/div/div[2]'
+                                              '/div/form/div[2]'
+                                              '/div/div/input')
             email_field.clear()
             email_field.send_keys(email)
             # Find and fill in the password field using XPath
             password_field = driver.find_element(By.XPATH,
-                                                 '/html/body/div[2]/div/div[2]/div/div[1]/div/div[2]/div/div[2]/div/form/div[3]/div/div[1]/div/input')
+                                                 '/html/body/div[2]'
+                                                 '/div/div[2]/div/div[1]'
+                                                 '/div/div[2]/div/div[2]'
+                                                 '/div/form/div[3]/div/'
+                                                 'div[1]/div/input')
             password_field.clear()
             password_field.send_keys(password)
 
@@ -47,14 +60,19 @@ class LoginAndSaveCookiesView(APIView):
 
             # Check if login was successful
             try:
-                after_login_element_xpath = '/html/body/div[2]/div/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/div[1]/div[1]/div[2]/div[3]/div[3]/div/button/div'
+                after_login_element_xpath = '/html/body/div[2]/div/' \
+                                            'div[2]/div[2]/div/div' \
+                                            '[2]/div/div[2]/div[2]/' \
+                                            'div[1]/div[1]/div[2]' \
+                                            '/div[3]/div[3]/div/' \
+                                            'button/div'
                 driver.find_element(By.XPATH,
-                                   after_login_element_xpath)  # Check for the element that indicates a successful login
+                                    after_login_element_xpath)
+                # that indicates a successful login
                 # Retrieve cookies from the Selenium session
                 cookies = driver.get_cookies()
-                # cookie_string = json.dumps(cookies)  # Convert cookies to a JSON string
                 return cookies
-            except Exception as e:
+            except Exception:
                 return None  # Login failed or the element wasn't found
         finally:
             driver.quit()
@@ -62,10 +80,12 @@ class LoginAndSaveCookiesView(APIView):
     def get_driver_requests(self, cookies):
         driver = wire_webdriver.Chrome(options=self.chrome_options)
         # Set cookies for the new session
-        driver.get(APOLLO_API_REQUEST_URL)# First, get the main page to set the domain
+        driver.get(APOLLO_API_REQUEST_URL)  # First,
+        # get the main page to set the domain
         for cookie in cookies:
             driver.add_cookie(cookie)  # Add each cookie to the driver
-        driver.get(APOLLO_API_REQUEST_URL)  # Navigate again to load resources with the cookies
+        driver.get(APOLLO_API_REQUEST_URL)  # Navigate again to load
+        # resources with the cookies
         # Access requests via the `requests` attribute
         time.sleep(30)
         return driver.requests
@@ -88,14 +108,20 @@ class LoginAndSaveCookiesView(APIView):
         return False
 
     def post(self, request):
-    # Get cookies after successful login
-        cookies = self.get_cookies(email=APOLLO_EMAIL, password=APOLLO_PASSWORD)
+        # Get cookies after successful login
+        cookies = self.get_cookies(email=APOLLO_EMAIL,
+                                   password=APOLLO_PASSWORD)
         if not cookies:
-            return JsonResponse({'error': 'Login failed. Please check your credentials.'},
+            return JsonResponse({'error': 'Login failed. '
+                                          'Please check your credentials.'},
                                 status=status.HTTP_401_UNAUTHORIZED)
         driver_requests = self.get_driver_requests(cookies=cookies)
-        is_writen = self.is_request_writen_to_db(cookies=cookies, driver_requests=driver_requests)
+        is_writen = self.is_request_writen_to_db(cookies=cookies,
+                                                 driver_requests=driver_requests)
         if is_writen:
-            return JsonResponse({'message': 'Request successfully written to the database.'},
+            return JsonResponse({'message': 'Request successfully '
+                                            'written to the database.'},
                                 status=status.HTTP_200_OK)
-        return JsonResponse({'message': 'The needed request was not found and information was not written'})
+        return JsonResponse({'message': 'The needed request was '
+                                        'not found and '
+                                        'information was not written'})
